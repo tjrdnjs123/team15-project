90bb92d1c5acec1606b957ea8e447ea858a49b96
banya <bjgeumgang@gmail.com>
2024-02-28 21:54:02 +0900

    feat: 버튼추가 및 기능구현 미완성2

--- scripts/record.js
+++ scripts/record.js
@@ -1,36 +1,66 @@
-/* 마이크 누르면 음성 인식이 시작된다 */
 let recognition = new webkitSpeechRecognition();
 recognition.lang = "ko-KR";
-const inputTask = document.getElementById("input-task");
+let inputTask = document.getElementById("inputTask");
 const fakeBtn = document.getElementById("fakeBtn");
+const micBtn = document.getElementById("micBtn");
 const stopBtn = document.getElementById("stopBtn");
+const btnClose = document.getElementById("btn-close");
 
-// 마이크버튼 클릭시
+/* 마이크버튼 클릭: 마이크 버튼사라짐 -> 녹음버튼나타남 */
 fakeBtn.addEventListener("click", () => {
-  fakeBtn.style.display = "none"; // 마이크버튼 사라짐
-  micBtn.style.display = "block"; // 녹음버튼 나타남
+  fakeBtn.style.display = "none";
+  micBtn.style.display = "block";
 });
 
-// 녹음버튼 클릭시
+/* 녹음버튼 클릭: 녹음버튼 사라짐 -> 중지버튼 나타남 */
 const record = (event) => {
   micBtn.style.display = "none"; // 녹음버튼 사라짐
   stopBtn.style.display = "block"; // 중지버튼 나타남
-  // 녹음시작
-  console.log("녹음시작");
+
   recognition.start();
-  // 녹음값 접근
+  console.log("녹음시작");
+  inputTask.value = "";
+
+  recognition.addEventListener("speechstart", () => {
+    console.log("인식중");
+  });
+  recognition.addEventListener("speechend", () => {
+    console.log("인식끝");
+  });
   recognition.addEventListener("result", (event) => {
     console.log(event);
-    // 녹음값 input value로 넣기
-    const transcript = event.results[0][0].transcript;
+    const transcript = event.results[0][0].transcript; // 녹음값 접근
     console.log(transcript);
-    inputTask.value = transcript;
+    inputTask.value = transcript; // 녹음값 input value로 반환
+    if (inputTask.value) {
+      fakeBtn.style.display = "block"; // 마이크버튼 나타남
+      stopBtn.style.display = "none"; // 중단버튼 사라짐
+    }
+  });
+
+  /* 녹음이 끝났을 때 입력창이 비어있으면 원상복귀 */
+  recognition.addEventListener("end", () => {
+    if (!inputTask.value) {
+      fakeBtn.style.display = "block"; // 마이크버튼 나타남
+      stopBtn.style.display = "none"; // 중단버튼 사라짐
+    }
   });
 };
-// 중지버튼 클릭시
+/* 중지버튼 클릭시 녹음이 중지된다 */
 stopBtn.addEventListener("click", () => {
   console.log("녹음중지");
   recognition.stop();
+  //   recognition.abort(); 논의 후 결정
+  stopBtn.style.display = "none"; // 중지버튼 사라짐
+  fakeBtn.style.display = "block"; // 마이크버튼 나타남
+});
+
+btnClose.addEventListener("click", () => {
+  inputTask.value = "";
+
+  fakeBtn.style.display = "block";
+  stopBtn.style.display = "none";
+  micBtn.style.display = "none";
 });
 // 시나리오1. 3개의 버튼을 만들어 기능을 각각넣는다
 // s1.1-1. 페이크이미지용 버튼을 만든다 V
@@ -38,7 +68,10 @@ stopBtn.addEventListener("click", () => {
 // s1.1-3. fakeBtn을 끌어온다 V
 // s1.1-4. 클릭시 display:none처리 V
 // s1.1-5. 스탑기능 구현 V
-// s1.1-6. isComplete 활용하여 녹음 <-> 중지 버튼간 토글형식으로 구현
-// s1.1-7. 하나의 함수 안에 동작하게끔하여 음성이 기록되면 input창을 제외하고 모두 초기화되어야한다
+// s1.1-6. 녹음의 상태가 끝나도 마이크로 돌아와야한다 V
+// s1.1-7. input값이 제출될 때 동시에 초기값설정. X버튼 클릭시는 그냥 초기값
+// s1.1-8. 하나의 함수 안에 동작하게끔하여 음성이 기록되면 input창을 제외하고 모두 초기화되어야한다
+
+// 해결할 오류1. 모달내 상태를 변경하고 제출, X버튼 누르고 다시 addtask버튼을 클릭하면 초기화 되지않았다.
 
 // s2.1.1. 버튼클릭시 이미지가 사라진다 -> 그자리에 새로운 이미지를 생성한다
