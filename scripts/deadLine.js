32c0646047203508e93da89bfda473ca9c2dc618
banya <bjgeumgang@gmail.com>
2024-03-01 19:39:09 +0900

    deadLine 초기화기능 추가

--- scripts/deadLine.js
+++ scripts/deadLine.js
@@ -1,6 +1,7 @@
 let timePicker = document.getElementById("time-picker");
 let deadWrap = document.getElementById("dead");
-timePicker.addEventListener("input", getTime);
+const btnClose = document.getElementById("btnClose");
+timePicker.addEventListener("change", getTime);
 
 function getTime() {
   if (timePicker.value === "") {
@@ -9,14 +10,8 @@ function getTime() {
     return timePicker.value;
   }
 }
-
-/*
-step1. 시간설정 창에서 시간, 분을 입력할 수 있다. V
-step2. 입력 후 제출을 클릭하면 '데드라인 00시00분 까지' 라는 문구가 task 하단에 표시된다
-  2-1. 입력받은 값을 조회할 수 있어야 한다.
-step3. 입력받은 시간을 값으로 저장해야한다.
-
-의문1. 시간표시하는 API가 있을까?
-아니면 단순히 input 2개로 구현해도 된다
-시간알림api는 어떤 데이터를 받는가? -> 형석님이 알아보고 답을 주실거임
-*/
+/* X버튼 클릭시 타임피커 초기화 */
+const resetTimePicker = () => {
+  timePicker.value = "";
+};
+document.querySelector(".btn-close").addEventListener("click", resetTimePicker);
