bd075e735744eabef4ad8110dd93b37aca08cf56
banya <bjgeumgang@gmail.com>
2024-03-01 18:42:49 +0900

    deadLine 미완성1

--- scripts/taskReset.js
+++ scripts/taskReset.js
@@ -5,20 +5,20 @@
 값이 있다면, 할일을 삭제하겠다는 알림창이 나오고, 선택여부에 따라 삭제 및 취소가 가능하다.
 */
 const taskReset = () => {
-  console.log(`새로고침클릭`);
-
-  if (localStorage.getItem(TODO_KEY)) {
-    // 로컬스토리지 내 값이 있다면?
-    const confirmMessage = confirm(`모든 할 일을 삭제하시겠습니까?`);
-    if (confirmMessage) {
-      localStorage.clear(); // 확인버튼 클릭시 전체삭제 진행
-      console.log(`전체삭제 실행`);
+    console.log(`새로고침클릭`);
+  
+    if (localStorage.getItem(TODO_KEY)) {
+      // 로컬스토리지 내 값이 있다면?
+      const confirmMessage = confirm(`모든 할 일을 삭제하시겠습니까?`);
+      if (confirmMessage) {
+        localStorage.clear(); // 확인버튼 클릭시 전체삭제 진행
+        console.log(`전체삭제 실행`);
+      } else {
+        console.log(`전체삭제 취소`); // 전체삭제 취소
+      }
     } else {
-      console.log(`전체삭제 취소`); // 전체삭제 취소
+      // 로컬스토리지 내 값이 없다면?
+      alert("삭제할 할 일이 없습니다!");
     }
-  } else {
-    // 로컬스토리지 내 값이 없다면?
-    alert("삭제할 할 일이 없습니다!");
-  }
-  render();
-};
+    render("doing");
+  };
