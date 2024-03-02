6f8d13e887d708fb53331ea5167e73848eef6168
banya <bjgeumgang@gmail.com>
2024-03-02 12:57:48 +0900

    deadLine 남은시간구현

--- scripts/deadLine.js
+++ scripts/deadLine.js
@@ -7,23 +7,3 @@ const resetTimePicker = () => {
   timePicker.value = "";
 };
 btnClose.addEventListener("click", resetTimePicker);
-
-// 실시간 시간 구하기
-function getClock() {
-  const date = new Date();
-  const hours = String(date.getHours()).padStart(2, "0"); // 실시간 시
-  const minutes = String(date.getMinutes()).padStart(2, "0"); // 실시간 분
-  // 타임피커의 문자열 시간쪽만
-  const pickerValueHour = timePicker.value.substr(0, 2).padStart(2, "0"); // 타임피커 시
-  const pickerValueMinute = timePicker.value.substr(3, 5).padStart(2, "0"); // 타임피커 분
-
-  // console.log("피커 시", timePicker.value.substr(0, 2));
-  // console.log("피커 분", timePicker.value.substr(3, 5));
-  // 절대값으로 시끼리, 분끼리의 결과를 출력
-  const realTime = `${pickerValueHour - hours} 시간 ${
-    pickerValueMinute - minutes
-  } 분 남았습니다`;
-  console.log(realTime);
-}
-
-addButton.addEventListener("click", getClock);
